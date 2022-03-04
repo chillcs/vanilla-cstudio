95f5e70e001e3206ca53fd39d14b38a4cd8f180f
Curtis <curtis@cstudio.ca>
2022-03-03 18:28:25 -0800

    mobile menu

--- logic.js
+++ logic.js
@@ -1,20 +1,34 @@
 //
 // Define Variables ---
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
 const slides = document.querySelectorAll('.slide');
 const btn01 = document.querySelector('.btn01');
 const btn02 = document.querySelector('.btn02');
 //
-// Portfolio Slide Btns ---
+// Menu Toggle ---
+menuBtn.addEventListener('click', () => {
+	menu.classList.toggle('menu-mob');
+});
+//
+// Portfolio Slide Buttons ---
 var slideCount = 0;
+if (slideCount === 0) {
+	btn01.classList.add('btn--dim');
+}
 btn01.addEventListener('click', () => {
 	slideCount--;
 	if (slideCount >= 0) {
 		slides[slideCount + 1].classList.remove('slide--active');
 		slides[slideCount].classList.add('slide--active');
-		btn02.classList.remove('btn--dim');
 	} else if (slideCount < 0) {
 		slideCount++;
+	}
+	if (slideCount === 0) {
 		btn01.classList.add('btn--dim');
+	}
+	if (slideCount < slides.length - 1) {
+		btn02.classList.remove('btn--dim');
 	}
 });
 btn02.addEventListener('click', () => {
@@ -22,9 +36,13 @@ btn02.addEventListener('click', () => {
 	if (slideCount < slides.length) {
 		slides[slideCount - 1].classList.remove('slide--active');
 		slides[slideCount].classList.add('slide--active');
-		btn01.classList.remove('btn--dim');
 	} else if (slideCount === slides.length) {
-		btn02.classList.add('btn--dim');
 		slideCount--;
+	}
+	if (slideCount === slides.length - 1) {
+		btn02.classList.add('btn--dim');
+	}
+	if (slideCount > 0) {
+		btn01.classList.remove('btn--dim');
 	}
 });
